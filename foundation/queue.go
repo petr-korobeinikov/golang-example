01e6ba75576cbf7c4d6850b9e6b76dbb70d04e1d
Petr Korobeinikov <pkorobeinikov@gmail.com>
2016-03-17 19:39:53 +0300

    Add Queue.Dequeue() impl.

--- foundation/queue.go
+++ foundation/queue.go
@@ -1,5 +1,7 @@
 package foundation
 
+import "errors"
+
 // Queue represents a "queue" structure.
 type Queue []interface{}
 
@@ -12,4 +14,17 @@ func (queue Queue) Len() (l int) {
 // Enqueue appends element to the queue.
 func (queue *Queue) Enqueue(element interface{}) {
 	*queue = append(*queue, element)
+}
+
+// Dequeue dequeues element from the queue.
+func (queue *Queue) Dequeue() (element interface{}, err error) {
+	q := *queue
+	l := q.Len()
+	if l == 0 {
+		err = errors.New("Can't dequeue element from an empty queue.")
+		return
+	}
+	element = q[0]
+	*queue = q[1:]
+	return
 }
--- foundation/queue_test.go
+++ foundation/queue_test.go
@@ -52,3 +52,39 @@ func TestQueueEnqueue(t *testing.T) {
 		}
 	}
 }
+
+func TestQueueDequeue(t *testing.T) {
+	// Arrange
+	cases := []struct {
+		expected Queue
+		given    Queue
+	}{
+		{Queue{}, Queue{1}},
+		{Queue{2, 3}, Queue{1, 2, 3}},
+	}
+
+	for _, c := range cases {
+		// Act
+		saveGiven := c.given
+		dequeued, _ := c.given.Dequeue()
+
+		// Assert
+		if !reflect.DeepEqual(c.expected, c.given) {
+			fmt := "%v.Dequeue() == %v, expected %v, dequeued %v"
+			t.Errorf(fmt, saveGiven, c.given, c.expected, dequeued)
+		}
+	}
+}
+
+func TestQueueDequeueFromEmptyQueue(t *testing.T) {
+	// Arrange
+	sut := &Queue{}
+
+	// Act
+	_, err := sut.Dequeue()
+
+	// Assert
+	if err == nil {
+		t.Errorf("Dequeuing from an empty queue must lead to an error.")
+	}
+}
